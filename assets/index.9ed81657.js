4c03456c9d826e6fecbf3348019f99947fed722d
vadiimvooo <108701488+vadiimvooo@users.noreply.github.com>
2023-10-07 08:19:41 +0000

    Deploying to gh-pages from @ vadiimvooo/nuxt-landing@cf00b806442df2a477cfd1ab215d0e4e3534dfe2 🚀

--- assets/index.9ed81657.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./entry.13bedd15.js";const o={};function r(t,s){return n(),c("div")}const a=e(o,[["render",r]]);export{a as default};
---
+++ assets/index.9ffec525.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.7e09e1b8.js";const o={};function r(t,s){return n(),c("div")}const a=e(o,[["render",r]]);export{a as default};
