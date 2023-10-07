d7b7b7ff1db46804692fc98027c1487c7fc5a423
Vadym <ASD4S.Profile@gmail.com>
2023-10-07 11:17:28 +0300

    Footer has been created.

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="bg-gray-primary">
+    <div class="max-w-[1440px] m-auto font-['Montserrat'] font-bold md:text-xl">
+      <div
+        class="grid grid-cols-3 h-[100px] place-content-center px-4 text-sm mob:text-xl mob:px-[50px] md:px-[100px] lg:px-[150px] m-auto"
+      >
+        <NuxtLink to="/" class="block w-[92px]">Contacts</NuxtLink>
+        <NuxtLink to="/" class="block justify-self-center">
+          Social Links
+        </NuxtLink>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
